fff411d3037670139acaf2333dc4fb6e71cbceb1
goddessIU <2581606394@qq.com>
2022-04-08 09:32:20 +0800

    4.8-1

--- miniVue/Vue3 code/learnCode.md
+++ miniVue/Vue3 code/learnCode.md
@@ -144,3 +144,49 @@ function patch(n1, n2) {
     }
 }
 ```
+
+基本的响应式
+```js
+let activeEffect
+class Dep {
+    constructor(value) {
+        this._value = value
+        this.subscribers = new Set()
+    }
+    get value() {
+        this.depend()
+        return this._value
+    }
+    set value(newVal) {
+        this._value = newVal
+        this.notify()
+    }
+    depend() {
+        if (activeEffect) {
+            this.subscribers.add(activeEffect)
+        }
+    }
+    notify() {
+        this.subscribers.forEach(effect => {
+            effect()
+        })
+    }
+}
+
+function watchEffect(effect) {
+    activeEffect = effect
+    activeEffect()
+    activeEffect = null
+}
+
+const msg = new Dep('hello')
+const ok = new Dep(true)
+
+watchEffect(() => {
+    if (ok.value) {
+        console.log(msg.value)
+    } else {
+        console.log('error')
+    }
+})
+```
--- test.js
+++ test.js
@@ -1,95 +1,43 @@
-function h(tag, props, children) {
-    return {
-        tag,
-        props,
-        children
+let activeEffect
+class Dep {
+    constructor(value) {
+        this._value = value
+        this.subscribers = new Set()
+    }
+    get value() {
+        this.depend()
+        return this._value
+    }
+    set value(newVal) {
+        this._value = newVal
+        this.notify()
+    }
+    depend() {
+        if (activeEffect) {
+            this.subscribers.add(activeEffect)
+        }
+    }
+    notify() {
+        this.subscribers.forEach(effect => {
+            effect()
+        })
     }
 }
 
-function mount(vnode, container) {
-    const el = vnode.el = document.createElement(vnode.tag)
-    if (vnode.props) {
-        for (const key in vnode.props) {
-        const value = vnode.props[key]
-        el.setAttribute(key, value)
-        }
-    }
-    if (vnode.children) {
-        if (typeof vnode.children === 'string') {
-            el.textContent = vnode.children
-        } else {
-            vnode.children.forEach(child => {
-                mount(child, el)
-            })
-        }
-    }
-    
-    
-    container.append(el)
+function watchEffect(effect) {
+    activeEffect = effect
+    activeEffect()
+    activeEffect = null
 }
 
-function patch(n1, n2) {
-    if (n1.tag === n2.tag) {
-        const el = n2.el = n1.el
+const msg = new Dep('hello')
+const ok = new Dep(true)
 
-        const oldProps = n1.props || {}
-        const newProps = n2.props || {}
-        for (let key in newProps) {
-            const oldValue = oldProps[key]
-            const newValue = newProps[key]
-            if (oldValue !== newValue) {
-                el.setAttribute(key, newValue)
-            }
-        }
-        for (let key in oldProps) {
-            if (!key in newProps) {
-                el.removeAttribute(key)
-            }
-        }
-
-
-        const oldChildren = n1.children
-        const newChildren = n2.children
-        if (typeof newChildren === 'string') {
-            if (typeof oldChildren === 'string') {
-                if (newChildren !== oldChildren) {
-                    el.textContent = newChildren
-                }
-            } else {
-                el.textContent = newChildren
-            }
-        } else {
-            if (typeof oldChildren === 'string') {
-                el.innerHTML = ''
-                newChildren.forEach(child => {
-                    mount(child, el)
-                })
-            } else {
-                const commonLenght = Math.min(oldChildren.length, newChildren.length) 
-                for (let i = 0; i < commonLenght; i++) {
-                    patch(oldChildren[i], newChildren[i])
-                }
-                if (newChildren.length > oldChildren.length) {
-                    newChildren.slice(commonLenght).forEach(child => {
-                        mount(child, el)
-                    })
-                } else if (newChildren.length < oldChildren.length) {
-                    oldChildren.slice(commonLenght).forEach(child => {
-                        el.removeElement(child.el)
-                    })
-                }
-            }
-        }
+watchEffect(() => {
+    if (ok.value) {
+        console.log(msg.value)
+    } else {
+        console.log('error')
     }
-}
-
-const vdom = h('div', { class: 'red' }, [
-    h('span', null, 'hello')
-])
-
-const vdom2 = h('div', { class: 'green' }, [
-    h('span', null, 'changed!')
-])
-
-mount(vdom, document.getElementById('app'))
-patch(vdom, vdom2)
+})
+ok.value = false
