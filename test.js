74327828046395a92931daf7d8d0527ff7e118bc
goddessIU <2581606394@qq.com>
2022-04-10 09:41:49 +0800

    4.10-1

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
 let activeEffect
 class Dep {
-    constructor(value) {
-        this._value = value
-        this.subscribers = new Set()
-    }
-    get value() {
-        this.depend()
-        return this._value
-    }
-    set value(newVal) {
-        this._value = newVal
-        this.notify()
-    }
+    subscribers = new Set()
     depend() {
         if (activeEffect) {
             this.subscribers.add(activeEffect)
@@ -30,14 +19,47 @@ function watchEffect(effect) {
     activeEffect = null
 }
 
-const msg = new Dep('hello')
-const ok = new Dep(true)
-
-watchEffect(() => {
-    if (ok.value) {
-        console.log(msg.value)
-    } else {
-        console.log('error')
+const targetMap = new WeakMap()
+function getDep(target, key) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
     }
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Dep()
+        depsMap.set(key, dep)
+    }
+    return dep
+}
+const reactiveHandlers = {
+    get(target, key, receiver) {
+        const dep = getDep(target, key)
+        dep.depend()
+        return Reflect.get(target, key, receiver)
+    },
+    set(target, key, value, receiver) {
+        const dep = getDep(target, key)
+        let res = Reflect.set(target, key, value, receiver)
+        dep.notify()
+        return res
+    }
+}
+
+function reactive(raw) {
+    return new Proxy(raw, reactiveHandlers)
+}
+
+
+const state = reactive({
+    count: 0
 })
-ok.value = false
+watchEffect(() => {
+    console.log(state.count)
+})
+state.count++
+watchEffect(() => {
+    console.log(state.msg)
+})
+state.msg = 'hi'
